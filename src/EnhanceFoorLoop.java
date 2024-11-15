1f51e5d9a2c824a25c91e9e4e16a98666fb22807
VRosa-jpg <vixkirosa@gmail.com>
2024-11-15 14:01:25 -0500

    Update”
    git commit -m Update”~

---
+++ src/EnhanceFoorLoop.java
@@ -0,0 +1,13 @@
+
+public class EnhanceFoorLoop {
+	
+     public static void main(String[] args) {
+    	 
+     String[] names = { "New York", "Dallas", "Las Vegas", "Florida" };
+           for (String name : names) {
+                 System.out.println(name);
+           	}
+           
+     }
+     
+}
